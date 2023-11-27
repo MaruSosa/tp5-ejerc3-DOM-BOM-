3567b850ac59899299538373c7ee0ced55f0c997
mariana <marianass712@gmail.com>
2023-11-27 19:43:30 -0300

    se realizo la logica para el funcionamiento de los botones

--- js/app.js
+++ js/app.js
@@ -4,42 +4,67 @@ const btnGuardar = document.getElementById("btnGuardar");
 btnGuardar.addEventListener("click", guardarTarea);
 
 function guardarTarea() {
-    const tareaNueva = document.getElementById("TareaNueva").value;
-    const listado = document.getElementById("listado");
-    
-    if (tareaNueva !== '') {
-        let existe = false;
+  const tareaNueva = document.getElementById("TareaNueva").value;
+  const listado = document.getElementById("listado");
 
-        listado.querySelectorAll('tbody tr').forEach((fila) => {
-            const textoTarea = fila.querySelector('td:nth-child(2)').textContent.trim();
+  if (tareaNueva !== "") {
+    let existe = false;
 
-            if (textoTarea === tareaNueva.trim()) {
-                existe = true;
+    listado.querySelectorAll("tbody tr").forEach((fila) => {
+      const textoTarea = fila
+        .querySelector("td:nth-child(2)")
+        .textContent.trim();
+
+      if (textoTarea === tareaNueva.trim()) {
+        existe = true;
+      }
+    });
+
+    if (existe) {
+      alert("La tarea ya existe en el listado.");
+    } else {
+      const nuevaFila = document.createElement("tr");
+      const tdNumeroTarea = document.createElement("td");
+      tdNumeroTarea.textContent = contadorTareas;
+      const tdTarea = document.createElement("td");
+      tdTarea.textContent = tareaNueva;
+      const tdAccionesBotones = document.createElement("td");
+
+      const btnEditar = document.createElement('button');
+      btnEditar.textContent = 'Editar';
+      btnEditar.addEventListener('click', () => {
+        const fila = btnEditar.parentElement.parentElement;
+        const tdTarea = fila.querySelector('td:nth-child(2)');
+        const textoTarea = tdTarea.textContent;
+        const inputEdicion = document.createElement('input');
+        inputEdicion.value = textoTarea; 
+        inputEdicion.classList.add('form-control'); 
+        tdTarea.textContent = '';
+        tdTarea.appendChild(inputEdicion);
+        inputEdicion.addEventListener('keypress', (e) => {
+            if (e.key === 'Enter') {
+                tdTarea.textContent = inputEdicion.value; 
             }
         });
+      });
 
-        if (existe) {
-            alert("La tarea ya existe en el listado.");
-        } else {
-            const nuevaFila = document.createElement('tr');
-            const tdNumeroTarea = document.createElement('td');
-            tdNumeroTarea.textContent = contadorTareas;
-            const tdTarea = document.createElement('td');
-            tdTarea.textContent = tareaNueva;
-            const tdAccionesBotones = document.createElement('td');
-            const btnEditar = document.createElement('button');
-            btnEditar.textContent = 'Editar';
-            const btnEliminar = document.createElement('button');
-            btnEliminar.textContent = 'Eliminar';
-            tdAccionesBotones.appendChild(btnEditar);
-            tdAccionesBotones.appendChild(btnEliminar);
-            nuevaFila.appendChild(tdNumeroTarea);
-            nuevaFila.appendChild(tdTarea);
-            nuevaFila.appendChild(tdAccionesBotones);
-            listado.querySelector('tbody').appendChild(nuevaFila);
-            contadorTareas++; 
-        }
-    }else{
-        alert("Ingrese una Tarea");
+      const btnEliminar = document.createElement("button");
+      btnEliminar.textContent = "Eliminar";
+      btnEliminar.addEventListener("click", function() {
+        this.parentElement.parentElement.remove();
+        contadorTareas--;
+      });
+
+      tdAccionesBotones.appendChild(btnEditar);
+      tdAccionesBotones.appendChild(btnEliminar);
+      nuevaFila.appendChild(tdNumeroTarea);
+      nuevaFila.appendChild(tdTarea);
+      nuevaFila.appendChild(tdAccionesBotones);
+      listado.querySelector("tbody").appendChild(nuevaFila);
+      contadorTareas++;
+      document.getElementById("TareaNueva").value = "";
     }
+  } else {
+    alert("Ingrese una Tarea");
+  }
 }
